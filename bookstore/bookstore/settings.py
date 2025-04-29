21e4bf0812534fc77b8b17dfc6f4e609b3008ce1
Anurag <anuragsingh8434845379@gmail.com>
2025-04-29 15:02:43 +0530

    a

--- bookstore/bookstore/settings.py
+++ bookstore/bookstore/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'books',  # Add the books app
+    'books','retailer'  # Add the books app
 ]
 
 MIDDLEWARE = [
